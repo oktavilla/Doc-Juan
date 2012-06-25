c2942a8784c4f823df0783c2e389a24c62a90188
Arvid Andersson <arvid.andersson@oktavilla.se>
2012-06-25 17:18:08 +0200

    Missing slash

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -12,8 +12,8 @@ timeout 30
 listen "#{@dir}/tmp/sockets/unicorn.sock", :backlog => 64
 
 # Set process id path
-pid "#{@dir}tmp/pids/unicorn.pid"
+pid "#{@dir}/tmp/pids/unicorn.pid"
 
 # Set log file paths
-stderr_path "#{@dir}log/unicorn.stderr.log"
-stdout_path "#{@dir}log/unicorn.stdout.log"
+stderr_path "#{@dir}/log/unicorn.stderr.log"
+stdout_path "#{@dir}/log/unicorn.stdout.log"
