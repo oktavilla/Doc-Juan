0e7a90cbcc6453bcd03109fc7594686602ae6ecf
Joel Junström <joel.junstrom@oktavilla.se>
2012-06-29 15:16:48 +0200

    Added DocJuan#logger

---
+++ lib/doc_juan/logger.rb
@@ -0,0 +1,23 @@
+require 'benchmark'
+
+module DocJuan
+  class << self
+    attr_accessor :logger
+  end
+
+  self.logger = Logger.new STDOUT
+
+  def self.log message
+    result = nil
+
+    if block_given?
+      ms = Benchmark.ms { result = yield }
+      logger.info '%s (%.1fms)' % [ message, ms ]
+    else
+      result = logger.info message
+    end
+
+    result
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,6 @@ require 'active_support/all'
 
 MiniTest::Unit.runner = MiniTest::SuiteRunner.new
 MiniTest::Unit.runner.reporters << MiniTest::Reporters::SpecReporter.new
+
+require_relative '../lib/doc_juan/logger'
+DocJuan.logger = Logger.new '/dev/null'
